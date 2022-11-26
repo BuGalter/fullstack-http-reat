b297eb61354885e6dc223fc9eb40372b512b9564
BuGalter <wardoc78@gmail.com>
2022-11-26 18:24:11 +0300

    feat: Added app header

--- calendar-react/src/components/AppHeader.js
+++ calendar-react/src/components/AppHeader.js
@@ -1,15 +1,32 @@
 import React from 'react';
+import '../styles/app-header.css';
+import Dropdown from './UI/Dropdown';
+import { users } from '../services/users';
 
 const AppHeader = () => {
   const appHeaderProps = {
-    text: 'Calendar with To-Do list'
+    text: 'Calendar with To-Do list',
+    dropdownText: 'Выберете пользователя',
   };
 
+  const activeUser = users.map((el) => {
+    if (el.isActive) {
+      return el.name;
+    }
+    return null;
+  });
+
   return (
-    <h1 className='app-header'>
-      { appHeaderProps.text }
-    </h1>
+    <div className='app-header'>
+      <h1 className='header'>{appHeaderProps.text}</h1>
+      {activeUser ? (
+        <h3>Пользователь: {activeUser}</h3>
+      ) : (
+        <h3>Пользователь: Неизвестный</h3>
+      )}
+      <Dropdown dropdownText={appHeaderProps.dropdownText} users={users} />
+    </div>
   );
-}
+};
 
 export default AppHeader;
