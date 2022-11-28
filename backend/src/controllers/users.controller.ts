74073f7589114c8f7a1578d459c74d6ea9b44ef2
BuGalter <wardoc78@gmail.com>
2022-11-28 10:03:19 +0300

    change: Change work logic in controllers user and weekdays

--- backend/src/controllers/users.controller.ts
+++ backend/src/controllers/users.controller.ts
@@ -2,23 +2,18 @@ import { User, } from '../models/User';
 import { UserService, } from '../services/users.service';
 
 export interface IUserController {
-  userService: UserService;
-  getOne(id: string): Promise<User>;
-  getAll(): Promise<User[]>;
+  getOne?(id: string): Promise<User>;
+  getAll?(): Promise<User[]>;
 }
 
 export class UserController implements IUserController {
-  userService: UserService;
-
-  constructor(userService: UserService) {
-    this.userService = userService;
+  static async getOne(id: string): Promise<User> {
+    const userService = new UserService();
+    return await userService.getOne(+id);
   }
 
-  async getOne(id: string): Promise<User> {
-    return await this.userService.getOne(+id);
-  }
-
-  async getAll(): Promise<User[]> {
-    return await this.userService.getAll();
+  static async getAll(): Promise<User[]> {
+    const userService = new UserService();
+    return await userService.getAll();
   }
 }
--- backend/src/controllers/week-days.controller.ts
+++ backend/src/controllers/week-days.controller.ts
@@ -2,18 +2,12 @@ import { IWeekDaysRepository, } from '../repositories/week-days.repositry';
 import { WeekDaysService, } from '../services/week-days.service';
 
 export interface IWeekDaysController {
-  weekDayService: WeekDaysService;
-  getAll(): Promise<IWeekDaysRepository[]>;
+  getAll?(): Promise<IWeekDaysRepository[]>;
 }
 
 export class WeekDaysController implements IWeekDaysController {
-  weekDayService: WeekDaysService;
-
-  constructor(weekDaysService: WeekDaysService) {
-    this.weekDayService = weekDaysService;
-  }
-
-  async getAll(): Promise<IWeekDaysRepository[]> {
-    return await this.weekDayService.getAll();
+  static async getAll(): Promise<IWeekDaysRepository[]> {
+    const weekDayService = new WeekDaysService();
+    return await weekDayService.getAll();
   }
 }
