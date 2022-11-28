d76c90445d57e44ef90db12230ad9e08a10f1176
BuGalter <wardoc78@gmail.com>
2022-11-28 10:14:48 +0300

    feat: Added to main server file test work with routes

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -1,10 +1,29 @@
 import * as http from 'http';
-import config from '../config/config';
+import config from './config/config';
+import routes from './routes';
 
-const requestListener = (req, res) => {
-  console.log(req);
-  res.writeHead(200);
-  res.end('My first server!');
+const requestListener: http.RequestListener = async (req, res) => {
+  const route = routes.find((el) => el.path === req.url);
+  if (route) {
+    const data = await route.controller();
+    const jsonData = JSON.stringify(data);
+    res.setHeader('Content-Type', 'application/json');
+    res.writeHead(200);
+    res.end(jsonData);
+  } else {
+    res.writeHead(404);
+    res.end('Bad request');
+  }
+  // const regexp = new RegExp(req.url);
+  /*   console.log(regexp);
+  console.log(routes);
+  let matches;
+  for (let i = 0; i < routes.length; i += 1) {
+    const route = routes[i].path;
+    matches = route.match(regexp);
+  }
+  console.log(matches);
+ */
 };
 
 const createServer = async () => {
@@ -14,8 +33,8 @@ const createServer = async () => {
 
 export const start = async () => {
   const server = await createServer();
-  const { port, host, prefix, } = config.server;
+  const { port, host, } = config.server;
   server.listen(port, host, () => {
-    console.log(`Server is running on http://${host}:${port}${prefix}`);
+    console.log(`Server is running on http://${host}:${port}`);
   });
 };
