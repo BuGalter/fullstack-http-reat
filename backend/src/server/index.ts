8733ece696cc7f1d1139b3c27641fbdc1badd939
BuGalter <wardoc78@gmail.com>
2022-11-28 13:10:52 +0300

    feat: Added to server header for cors requests

--- backend/src/server/index.ts
+++ backend/src/server/index.ts
@@ -8,22 +8,13 @@ const requestListener: http.RequestListener = async (req, res) => {
     const data = await route.controller();
     const jsonData = JSON.stringify(data);
     res.setHeader('Content-Type', 'application/json');
+    res.setHeader('Access-Control-Allow-Origin', '*');
     res.writeHead(200);
     res.end(jsonData);
   } else {
     res.writeHead(404);
     res.end('Bad request');
   }
-  // const regexp = new RegExp(req.url);
-  /*   console.log(regexp);
-  console.log(routes);
-  let matches;
-  for (let i = 0; i < routes.length; i += 1) {
-    const route = routes[i].path;
-    matches = route.match(regexp);
-  }
-  console.log(matches);
- */
 };
 
 const createServer = async () => {
